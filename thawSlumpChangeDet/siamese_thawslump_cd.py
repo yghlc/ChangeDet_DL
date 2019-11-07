7931e4d504deafcb4bfb0672301d8ee245422f25
Lingcao Huang <huanglingcao@gmail.com>
2019-11-07 16:43:50 +0800

    typo fix

--- thawSlumpChangeDet/siamese_thawslump_cd.py
+++ thawSlumpChangeDet/siamese_thawslump_cd.py
@@ -22,7 +22,7 @@ import random
 import rasterio
 import numpy as np
 
-# add the update level path for importing dataTools model
+# add the upper level path for importing dataTools model
 script_folder = os.path.dirname(sys.argv[0])
 upper_folder = os.path.dirname(script_folder)
 sys.path.insert(0, upper_folder)
