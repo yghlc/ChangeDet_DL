f8a3f1c46303abe05a8fd5c641363e3311fa929d
Lingcao Huang <huanglingcao@gmail.com>
2020-01-20 16:22:02 +0800

    typo fix

--- thawSlumpChangeDet/bash_train_siamese_cd.py
+++ thawSlumpChangeDet/bash_train_siamese_cd.py
@@ -39,7 +39,7 @@ learning_rate = parameters.get_string_parameters(para_file, 'base_learning_rate'
 weight_decay = parameters.get_string_parameters(para_file, 'weight_decay')
 
 if os.path.isdir(expr_name):
-    print('%s already exist, backup it'%expr_name)
+    print('%s already exists, backup it'%expr_name)
     import glob
     backups = glob.glob(expr_name + '_[0-9]*')
     if len(backups) < 1:
