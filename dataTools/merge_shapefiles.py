fffe94cbbfb4a77414ad9bea255d0a394fc253aa
Lingcao Huang <huanglingcao@gmail.com>
2020-06-10 21:41:19 +0800

    convert to polygons, merge polygons in different shapefiles

--- dataTools/merge_shapefiles.py
+++ dataTools/merge_shapefiles.py
@@ -11,9 +11,62 @@ add time: 08 June, 2020
 
 
 import os, sys
+sys.path.insert(0,os.path.expanduser('~/codes/PycharmProjects/DeeplabforRS'))
+
+import basic_src.io_function as io_function
+import basic_src.basic as basic
+
+from basic_src.map_projection import get_raster_or_vector_srs_info_proj4
+import vector_gpd
+
+import pandas as pd
+import geopandas as gpd
+
 
 def merge_shape_files(file_list, save_path):
-    pass
+
+    if len(file_list) < 1:
+        raise IOError("no input shapefiles")
+
+    ref_prj = get_raster_or_vector_srs_info_proj4(file_list[0])
+
+    # read polygons as shapely objects
+    attribute_names = None
+    polygons_list = []
+    polygon_attributes_list = []
+
+    for idx, shp_path in enumerate(file_list):
+
+        # check projection
+        prj = get_raster_or_vector_srs_info_proj4(file_list[idx])
+        if prj != ref_prj:
+            raise ValueError('Projection inconsistent: %s is different with the first one'%shp_path)
+
+        shapefile = gpd.read_file(shp_path)
+
+        # go through each geometry
+        for ri, row in shapefile.iterrows():
+            if idx == 0 and ri==0:
+                attribute_names = row.keys().to_list()
+                attribute_names = attribute_names[:len(attribute_names) - 1]
+                # basic.outputlogMessage("attribute names: "+ str(row.keys().to_list()))
+
+            polygons_list.append(row['geometry'])
+            polygon_attributes = row[:len(row) - 1].to_list()
+            polygon_attributes_list.append(polygon_attributes)
+
+    # save results
+    save_polyons_attributes = {}
+    for idx, attribute in enumerate(attribute_names):
+        # print(idx, attribute)
+        values = [item[idx] for item in polygon_attributes_list]
+        save_polyons_attributes[attribute] = values
+
+    save_polyons_attributes["Polygons"] = polygons_list
+    polygon_df = pd.DataFrame(save_polyons_attributes)
+
+
+    return vector_gpd.save_polygons_to_files(polygon_df, 'Polygons', ref_prj, save_path)
 
 
 if __name__ == "__main__":
--- thawSlumpChangeDet/bash_postProc.py
+++ thawSlumpChangeDet/bash_postProc.py
@@ -11,53 +11,91 @@ add time: 08 June, 2020
 
 import os,sys
 sys.path.insert(0,os.path.expanduser('~/codes/PycharmProjects/DeeplabforRS'))
+cd_dir=os.path.expanduser('~/codes/PycharmProjects/ChangeDet_DL')
+sys.path.insert(0,cd_dir)
 
 import parameters
 import basic_src.io_function as io_function
+import basic_src.basic as basic
+
+from dataTools.merge_shapefiles import merge_shape_files
 
 # get parameter
 para_file=sys.argv[1]
 if os.path.isfile(para_file) is False:
     raise IOError('File %s not exists in current folder: %s'%(para_file, os.getcwd()))
 
-cd_dir=os.path.expanduser('~/codes/PycharmProjects/ChangeDet_DL')
+
 
 # convert all the images to shape files
 root=os.getcwd()
+
+# image pair for change detections (prediction)
 img_pair_txt=os.path.join(root,'pair_images_list.txt')
+
+# images pairs and change polygons for training
+changeDet_training_txt=os.path.join(root,'changeDet_multi_training_files.txt')
 inf_result_dir = parameters.get_string_parameters(para_file, 'inf_result_dir')
 
 
-def post_processing_one_change_map(idx, output_dir):
+def post_processing_one_change_map(idx, nodata=None):
 
     # get the results
+    change_map_tifs = io_function.get_file_list_by_pattern(inf_result_dir,'*_%d.tif'%idx)
+    if len(change_map_tifs) != 1:
+        raise ValueError("more than one tif (*%d.tif) in %s"%(idx, inf_result_dir))
+
+    change_map_tif = change_map_tifs[0]
+
+
+    shp_dir = os.path.join(inf_result_dir, os.path.splitext(os.path.basename(change_map_tif))[0])
+    out_shp = os.path.join(shp_dir,'%d.shp'%idx)
+    if os.path.isfile(out_shp):
+        basic.outputlogMessage("%s exists, skip"%out_shp)
+        return out_shp
+
+    io_function.mkdir(shp_dir)
+
+    #set no change pixels (0) as no data
+    if nodata is not None:
+        command_string = 'gdal_edit.py -a_nodata %d %s' % (nodata, change_map_tif)
+        output = os.system(command_string)
+        if output != 0:
+            sys.exit(1)  # this can help exit the bash script
 
     # to shape files
-    command_string = "gdal_polygonize.py -8 "
-
+    command_string = 'gdal_polygonize.py -8 %s -b 1 -f "ESRI Shapefile" %s'%(change_map_tif,out_shp)
+    print(command_string)
     output = os.system(command_string)
     if output != 0:
         sys.exit(1)  # this can help exit the bash script
 
-    # merge the shape file to a single one
+    return out_shp
 
-    # remove small polygons
-
-    # calculate IOU values (need validation polygons)
-
-
+# convert change map (raster) to shape file, one by one
+change_shp_list = []
 with open(img_pair_txt,'r') as f_obj:
-
-
     lines = f_obj.readlines()
-    for str_line in lines:
+    for idx, str_line in enumerate(lines):
         # path_str [old_image, new_image, label_path (if available)]
         path_str = [os.path.join(root, item.strip()) for item in str_line.split(':')]
 
-        # post_processing_one_change_map
+        # post_processing_one_change_map, set no change pixels (0) as no data
+        change_polygons_shp = post_processing_one_change_map(idx, nodata=0)
+        change_shp_list.append(change_polygons_shp)
 
     pass
 
+# merge the shape file to a single one
+save_path = os.path.join(inf_result_dir,'predict_change_polygons.shp')
+if merge_shape_files(change_shp_list,save_path) is True:
+    basic.outputlogMessage("save change polygons to %s"%save_path)
+else:
+    basic.outputlogMessage("error, saving change polygons failed")
+
+# remove small polygons
+
+# calculate IOU values (need validation polygons)
 
 
 
