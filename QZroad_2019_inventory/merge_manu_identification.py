ae6eb210b7ab69140d435ab8d970ba2a2bcb7cc0
Lingcao Huang <huanglingcao@gmail.com>
2020-07-29 21:29:10 +0800

    v3

--- QZroad_2019_inventory/merge_manu_identification.py
+++ QZroad_2019_inventory/merge_manu_identification.py
@@ -14,20 +14,31 @@ sys.path.insert(0,os.path.expanduser('~/codes/PycharmProjects/ChangeDet_DL/dataT
 
 from merge_shapefiles import merge_shape_files
 
+sys.path.insert(0,os.path.expanduser('~/codes/PycharmProjects/DeeplabforRS'))
+import vector_gpd
+
 shp_dir = os.path.expanduser('~/Data/Qinghai-Tibet/QZrailroad_buffer_area/thaw_slumps_for_training')
 
 # would add more if there are more manual identification results.
 shp1 = os.path.join(shp_dir, 'blh_manu_RTS_utm_201907_08.shp')
 shp2 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp1_iter30000_QZroad_2019_manu_edit.shp')
-shp_paths = [shp1, shp2]
+shp3 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp2_iter30000_QZroad_2019_NEW_polygons_edit.shp')
+shp_paths = [shp1, shp2, shp3]
 
 
 #the version 1 is derived from "blh_manu_RTS_utm_201907_08.shp"
-output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v2.shp')
+# output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v2.shp')
+
+# in qz_manu_RTS_utm_201907_08_v3, there are two polygons are close to another one and intersect with others, not duplicated
+# edit it to avoid this issue, and save to qz_manu_RTS_utm_201907_08_v3_edit.shp
+output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v3.shp')
+# output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v3_edit.shp')
 
 merge_shape_files(shp_paths, output)
 
 #TODO: check any duplicated polygons
+vector_gpd.find_polygon_intersec_polygons(output)
+
 
 
 
