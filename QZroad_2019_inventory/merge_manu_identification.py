daa163d3e4bfe85ca87c6bc571e3cb7c3752a038
Lingcao Huang <huanglingcao@gmail.com>
2020-07-31 19:49:54 +0800

    version 4

--- QZroad_2019_inventory/merge_manu_identification.py
+++ QZroad_2019_inventory/merge_manu_identification.py
@@ -20,10 +20,12 @@ import vector_gpd
 shp_dir = os.path.expanduser('~/Data/Qinghai-Tibet/QZrailroad_buffer_area/thaw_slumps_for_training')
 
 # would add more if there are more manual identification results.
-shp1 = os.path.join(shp_dir, 'blh_manu_RTS_utm_201907_08.shp')
-shp2 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp1_iter30000_QZroad_2019_manu_edit.shp')
-shp3 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp2_iter30000_QZroad_2019_NEW_polygons_edit.shp')
-shp_paths = [shp1, shp2, shp3]
+# shp1 = os.path.join(shp_dir, 'blh_manu_RTS_utm_201907_08.shp')
+# shp2 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp1_iter30000_QZroad_2019_manu_edit.shp')
+# shp3 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp2_iter30000_QZroad_2019_NEW_polygons_edit.shp')
+shp_1_2_3 = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v3_edit.shp')
+shp4 = os.path.join(shp_dir, 'QZ_deeplabV3+_2_exp3_iter30000_QZroad_2019_NEW_polygons_edit.shp')
+shp_paths = [shp_1_2_3, shp4]
 
 
 #the version 1 is derived from "blh_manu_RTS_utm_201907_08.shp"
@@ -31,8 +33,9 @@ shp_paths = [shp1, shp2, shp3]
 
 # in qz_manu_RTS_utm_201907_08_v3, there are two polygons are close to another one and intersect with others, not duplicated
 # edit it to avoid this issue, and save to qz_manu_RTS_utm_201907_08_v3_edit.shp
-output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v3.shp')
+# output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v3.shp')
 # output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v3_edit.shp')
+output = os.path.join(shp_dir, 'qz_manu_RTS_utm_201907_08_v4.shp')
 
 merge_shape_files(shp_paths, output)
 
